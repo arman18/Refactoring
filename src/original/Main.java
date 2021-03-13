815a58a77bb1a6c8ef5fb818e8feb96738d6240d
ahcArman <bsse1029@iit.du.ac.bd>
2021-03-13 09:48:58 +0600

    initial project

---
+++ src/original/Main.java
@@ -0,0 +1,77 @@
+package original;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main {
+    static JFrame Frame = new JFrame("Game");   //delcaring JFrame out of main method for easier access later
+    static boolean parallaxscrolling = false;
+
+    public static void main(String[] args) {                        //making a main menu
+        if(args.length > 0){
+            parallaxscrolling = true;
+//            System.out.println("true");
+        }
+        GridLayout layout = new GridLayout(0,1);         //buttons in grid layout
+        JPanel Menu = new JPanel();                                 //initialising the Menu Panel
+        JButton slow = new JButton("Slow");                     //defining buttons
+        JButton medium = new JButton("Medium");
+        JButton fast = new JButton("Fast");
+        JFrame fr = new JFrame();
+        Menu.setLayout(layout);
+        Menu.add(slow);
+        Menu.add(medium);
+        Menu.add(fast);
+
+
+        slow.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent e){
+                fr.dispose();                                       //starting the game with PlayerSpeed 1
+                game(1);
+            }
+        });
+
+        medium.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                fr.dispose();                                       //starting the game with PlayerSpeed up = 2, down =1
+                game(2);
+            }
+        });
+
+        fast.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                fr.dispose();                                       //starting the game with PlayerSpeed 2
+                game(3);
+
+            }
+        });
+
+        fr.add(new MenuIcon(), BorderLayout.CENTER);            //adding graphics
+        fr.add(Menu, BorderLayout.SOUTH);                           //adding buttons
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      //initializing basic stuff like size and position
+        fr.setSize(300, 300);
+        fr.setLocationRelativeTo(null);
+        fr.setVisible(true);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void game(int PlayerSpeed){
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //initializing basic stuff like position and size
+        Frame.setSize(800,400);
+        Frame.add(new MySnake(PlayerSpeed));                       //add the custom JPanel
+        Frame.setIconImage(new ImageIcon("src/com/sebi/dot.png").getImage());   //trying to add an ImageIcon, succeeded in my IDE, but as soon as I compiled the Jar, it stopped working
+        Frame.setResizable(false);
+        Frame.setVisible(true);             //setVisible AFTER adding the JPanel
+        Frame.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/original/MySnake.java
@@ -0,0 +1,460 @@
+package original;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
+public class MySnake extends JPanel implements ActionListener {
+                                                        //init Objects
+    private Player me = new Player();
+    private Hindernis hind = new Hindernis(1);
+    private Hindernis hind2 = new Hindernis(2);
+    private Hindernis hind3 = new Hindernis(3);
+    private Health health = new Health();
+    private Sun sun = new Sun();
+    private BufferedImage bg1;
+    private BufferedImage bg2;
+    private int bgp1 = 0;
+    private int bgp2 = 0;
+    private int fx = 0;
+
+    private Timer timer;
+    private Color backgr;
+    private JFrame debuggConsole = new JFrame("console");
+    private JTextArea debugDisplay = new JTextArea();
+
+    private boolean imune = false;                      //init variables
+    private boolean gameover = false;
+    private boolean darker = false;
+    private float bright = 0.97f;
+    private int sunIdle = 0;
+    private int iters = 0;
+    private int count = 0;
+    private int upspeed;
+    private int downspeed;
+    enum SUNSTATE {sunUp, sunDown, moonUp, moonDown}
+
+
+    MySnake(int PlayerSpeed){
+        try{
+            URL resource = MySnake.class.getResource("bg1.png");
+            bg1 = ImageIO.read(resource);
+            URL resource2 = MySnake.class.getResource("bg2.png");
+            bg2 = ImageIO.read(resource2);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        if(PlayerSpeed == 1){upspeed = downspeed = 1;}
+        if(PlayerSpeed == 2){upspeed = 2; downspeed = 1;}
+        if(PlayerSpeed == 3){upspeed = 2; downspeed = 2;}
+        backgr = Color.getHSBColor(0.6f, 0.8f, 0.97f/2);
+        setBackground(backgr);                                    //init JPanel properties
+        setFocusable(true);
+        requestFocusInWindow();
+        addKeyListener(new TAdapter());
+        setPreferredSize(new Dimension(800, 400));
+        initConsole();
+        hind.Hx = 600;
+        hind2.Hx = 800;
+        hind3.Hx = 1200;
+        timer = new Timer(5, this);
+        timer.start();
+    }
+
+
+    private void initConsole(){                                 //making a console Frame with Text
+        debuggConsole.setResizable(false);
+        debuggConsole.setLocation(10, 10);
+        debugDisplay.setAlignmentY(JLabel.TOP_ALIGNMENT);
+        debugDisplay.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+        debugDisplay.setOpaque(true);
+        debugDisplay.setEditable(false);
+        debugDisplay.setText("");
+        debugDisplay.setForeground(Color.GREEN);
+        debugDisplay.setBackground(Color.BLACK);
+        debugDisplay.setFont(new Font("Unispace", Font.BOLD, 13));
+        debugDisplay.setLineWrap(true);
+        debuggConsole.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        debuggConsole.add(debugDisplay);
+        debuggConsole.setVisible(false);
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
+        g.setColor(sun.color);
+        g.fillOval(sun.x - sun.size, sun.y - sun.size, sun.size, sun.size);
+        if( Main.parallaxscrolling ) {
+            g.drawImage(bg1, bgp1, -80, this);
+            g.drawImage(bg1, bgp1 + 800, -80, this);
+            g.drawImage(bg2, bgp2, -80, this);
+            g.drawImage(bg2, bgp2 + 800, -80, this);
+        }
+        g.setColor(Color.yellow);
+        g.fillRect(me.x, me.y, 10, 10);                   //Player rendern
+
+        int abstnd = 30;
+        for (int i = 0; i < health.count; i++){                                 //Herzen rendern
+            int[] xPoints = {i * abstnd, i * abstnd,  12+i*abstnd, 15+i*abstnd,
+                    27+i*abstnd, 27+i*abstnd, 23+i*abstnd, 16+i*abstnd, 16+i*abstnd,
+                    11+i*abstnd, 11+i*abstnd , 4+i*abstnd, i * abstnd};
+            int[] yPoints = {5, 15, 26, 26, 15,  5,  0,  0,  1, 1,  0  , 0, 5};
+            g.setColor(Color.decode("#C91010"));
+            g.fillPolygon(xPoints, yPoints, 13);
+        }
+
+        g.setColor(Color.green);
+        g.fillRect(hind.Hx, hind.Hy, hind.Hw, 400);       //Hindernisse Rendern
+        g.setColor(Color.green.darker());
+        g.fillRect(hind2.Hx, hind2.Hy, hind2.Hw, 400);
+        g.setColor(Color.green.darker().darker());
+        g.fillRect(hind3.Hx, hind3.Hy, hind3.Hw, 400);
+        g.setColor(Color.orange);
+        g.fillRect(0, 320, 800, 399);                     //Boden rendern
+        if(Main.parallaxscrolling) {
+            g.drawImage(bg2, fx, -20, this);
+            g.drawImage(bg2, fx + 800, -20, this);
+        }
+
+
+        if(gameover){
+            String msg = "Game Over";
+            String msg2 = "Your score: " + count;
+            Font small = new Font("Helvetica", Font.BOLD, 14);
+            FontMetrics metr = getFontMetrics(small);
+
+            g.setColor(Color.white);
+            g.setFont(small);
+            g.drawString(msg, (800 - metr.stringWidth(msg)) / 2, 400 / 2 - 9);
+            g.drawString(msg2, (800 - metr.stringWidth(msg2)) / 2, 400 / 2 + 9);
+        }
+
+    }
+
+
+    private boolean collision(){
+        if (hind.Hx < me.x + 10 && me.x + 10 < hind.Hx + hind.Hw){      //check for each obstacle
+            if (me.y + 11 >= hind.Hy){
+                return true;
+            }
+        }
+        if (hind2.Hx < me.x + 10 && me.x + 10 < hind2.Hx + hind2.Hw){
+            if (me.y + 11 >= hind2.Hy){
+                return true;
+            }
+        }
+        if (hind3.Hx < me.x + 10 && me.x + 10 < hind3.Hx + hind3.Hw){
+            return me.y + 11 >= hind3.Hy;
+        }
+
+        return false;
+    }
+
+
+    private void doStuff(){
+        count++;
+                                                //nach einiger Zeit immer schneller werden
+        if (5 - count/10000 >= 0){
+            timer.setDelay((5 - count/10000));
+        }
+                                        //Hintergrund und Tageszeit
+        //if(count % 1 == 0){
+            if (bright <= 0.2684843){
+                darker = true;
+            }
+
+            if (bright >= 0.97f){
+                darker = false;
+                //-1781
+            }
+
+            if (darker){
+                bright += 0.0005/2;
+            } else {
+                bright -= 0.0005;
+            }
+
+            backgr = Color.getHSBColor(0.6f, 0.8f, bright);
+            setBackground(backgr);
+
+
+        //}
+                                                    //sonnenzyklus
+
+        if(count % 1.5 == 0){
+            if ( sun.state == SUNSTATE.sunUp && sunIdle < 1){ sun.y -= 1; sun.x -= 1;}
+            if ( sun.state == SUNSTATE.sunDown){ sun.y += 1; sun.x -= 1;}
+            if ( sun.state == SUNSTATE.moonUp  && sunIdle < 1){ sun.y -= 1; sun.x -= 1;}
+            if ( sun.state == SUNSTATE.moonDown){sun.y += 1; sun.x -= 1;}
+            if ( sunIdle > 0){
+                sun.x -= 1;
+                if (sun.x <= 315){
+                    sunIdle = 0;
+                    if (sun.state == SUNSTATE.sunUp){sun.state = SUNSTATE.sunDown;}
+                    if (sun.state == SUNSTATE.moonUp){sun.state = SUNSTATE.moonDown;}
+                }
+            }
+        }
+
+        if(count % 4 == 0){
+            bgp1 -= 1;
+        }
+        if(bgp1 == -800){
+            bgp1 = 0;
+        }
+        if(count % 2 == 0){
+            bgp2 -= 1;
+        }
+        if(bgp2 == -800){
+            bgp2 = 0;
+        }
+        fx--;
+        if(fx == -800){
+            fx = 0;
+        }
+
+
+
+
+                                                //jump mechanic
+
+        if(me.up && me.y >= 250){
+            me.setY(me.y - upspeed);
+        }
+
+        if (me.y == 250){
+            me.up = false;
+            me.down = true;
+        }
+
+        if (me.down && me.y < 310){
+            me.setY(me.y + downspeed);
+        }
+
+        if(me.down && me.y == 310){
+            me.down = false;
+        }
+
+        hind.Hx -= 1;                                   //Hindernisse bewegen
+        hind2.Hx -= 1;
+        hind3.Hx -= 1;
+
+        if ((hind.Hx + hind.Hw) < 0){                       //wenn hindernisse aus dem Bild, dann neue Position generieren
+            hind.Hx = generateNewPosition(1);
+            hind.Hy = 300 - ((int) (Math.random() * 7))*3;
+        }
+
+        if ((hind2.Hx + hind2.Hw) < 0){
+            hind2.Hx = generateNewPosition(2);
+            hind2.Hy = 300 - ((int) (Math.random() * 7))*3;
+        }
+
+        if ((hind3.Hx + hind3.Hw) < 0){
+            hind3.Hx = generateNewPosition(3);
+            hind3.Hy = 300 - ((int) (Math.random() * 7))*3;
+        }
+
+        if (collision()) {                  //player fuer 12 ticks imun machen
+            if (!imune) {
+                health.count -= 1;
+                imune = true;
+                iters = count;
+            }
+        }
+
+        if (count - iters == 12){
+            imune = false;
+        }
+
+        if (health.count < 1){      //wenn tot, gameOver
+            gameOver();
+        }
+
+
+        if (sun.state == SUNSTATE.sunUp && sun.y - sun.size == 30){   //sonne oben angekommen
+            sunIdle = 1;
+        } else {
+
+            if (sun.state == SUNSTATE.sunDown && sun.y + sun.size >= 400){  //sonne unten angekommen
+                sun.state = SUNSTATE.moonUp;
+                sun.color = Color.white;
+                sun.x = 800;
+                sun.y = 400;
+            } else {
+
+                if (sun.state == SUNSTATE.moonUp && sun.y - sun.size == 30){     //moon oben angekommen
+                    sunIdle = 1;
+                } else {
+
+                    if (sun.state == SUNSTATE.moonDown && sun.y - sun.size == 400){ //moon unten angekommen
+                        sun.state = SUNSTATE.sunUp;
+                        sun.color = Color.yellow;
+                        sun.x = 800;
+                        sun.y = 400;
+                    }
+
+                }
+            }
+        }     //endof ifs
+                                                                                //update Text in Console
+        debugDisplay.setText("H1: " + hind.Hx + System.lineSeparator()
+                + "H2: " + hind2.Hx + System.lineSeparator()
+                + "H3: " + hind3.Hx + System.lineSeparator()
+				+ "S:  " + count + System.lineSeparator()
+                + "P:  " + me.y + System.lineSeparator()
+                + "HP: " + health.count + System.lineSeparator()
+                + "DN: " + darker + System.lineSeparator()
+                + "p1: " + bgp1 + System.lineSeparator()
+                + "p2: " + bgp2 + System.lineSeparator()
+        );
+
+    }
+
+
+    private int generateNewPosition(int id){
+        return (int)(Math.random() * 100) + 800+800*id;
+    }
+
+
+    private void gameOver(){
+        timer.stop();
+        gameover = true;
+    }
+        
+    static int cnt=0;
+    @Override
+    public void actionPerformed(ActionEvent e){
+        doStuff();
+        repaint();
+        System.out.println(cnt++);
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+
+            if(e.getKeyCode() == KeyEvent.VK_NUMPAD7){
+
+                if(!debuggConsole.isVisible()){
+                    debuggConsole.setSize(300, 400);
+                    debuggConsole.setVisible(true);
+                } else {
+                    debuggConsole.setVisible(false);
+                }
+
+                Main.Frame.toFront();
+
+            } else {
+                if (e.getKeyCode() == KeyEvent.VK_P){
+                    if(timer.isRunning()){timer.stop();} else {timer.restart();}
+                }else {
+                    me.jump();
+                }
+            }
+
+            if(gameover && e.getKeyCode() == KeyEvent.VK_ENTER){
+                count = 0;
+                iters = 0;
+                gameover = false;
+                health.count = 4;
+                timer.start();
+            }
+
+            if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                System.exit(0);
+            }
+
+        }
+
+    }
+
+    class Player{
+
+        int x;
+        int y;
+        boolean up;
+        boolean down;
+        Image icon;
+
+
+        Player(){
+            x = 400;
+            y = 310;
+            up = false;
+            down = false;
+            icon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("src/com/sebi/dot.png")).getImage();
+        }
+
+        void jump() {
+            if (!this.up && !this.down) {
+                this.up = true;
+            }
+
+        }
+
+        /*void setX(int _x){      //probably redundant bc it's never used
+            this.x = _x;
+        }*/
+
+        void setY(int _y){
+            this.y = _y;
+        }
+
+    }
+
+
+    class Hindernis{
+
+        int Hx;
+        int Hy;
+        int Hw;
+
+        Hindernis(int id){
+            Hx = (int)(Math.random() * 100) + 800+id*800;
+            Hy = 300 - ((int) (Math.random() * 7))*3;
+            Hw = 12;
+        }
+
+    }
+
+
+    class Sun{
+
+        int x;
+        int y;
+        int size;
+        Color color;
+        SUNSTATE state;
+
+        Sun(){
+            x = 800;
+            y = 400;
+            size = 50;
+            state = SUNSTATE.moonUp;
+            color = Color.white;
+        }
+
+    }
+
+
+    class Health{
+
+        int count;
+        Image icon;
+
+        Health(){
+            count = 4;
+            icon = new ImageIcon("src/com/sebi/heart.png").getImage(); //TODO: getClass.getResource()
+        }
+
+    }
+
+}
